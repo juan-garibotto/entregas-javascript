2b7ac564256138bd181d212899784fcb3844a28d
juan.garibotto <juanmag2291@gmail.com>
2024-01-11 20:41:07 -0300

    correcciones 3

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
-let nombre = prompt("Ingrese su nombre");
-let apellido = prompt("Ingrese su apellido");
+let nombre ;
+let apellido ;
+let titular;
 
-alert( "Hola" + " " + nombre + " " + apellido + "!");
+while (true) {
+    nombre = prompt("Ingrese su nombre");
+    apellido = prompt("Ingrese su apellido");
 
-let titular = prompt("¿Es usted el/la titular de la tarjeta?");
+    alert("Hola " + nombre + " " + apellido + "!");
 
-while (titular.toLowerCase() !== "si") {
-    alert("Solo el titular puede realizar la compra.");}
+    titular = prompt("¿Es usted el/la titular de la tarjeta?");
+
+    if (titular.toLowerCase() === "si") {
+        alert("Pulse enter para continuar");
+        break;
+    } else if (titular.toLowerCase() === "no") {
+        alert("Solo el titular puede realizar la compra.");
+        
+    } else {
+        alert("Respuesta no válida. Por favor, ingrese 'si' o 'no'.");
+    }
+}
+
 
 function cuotasTarjeta(montoTotalTarjeta, pagos) {
-    var cuotaMensual = montoTotalTarjeta / pagos;
-    return cuotaMensual;}
+    let cuotaMensual = montoTotalTarjeta / pagos;
+    return cuotaMensual;
+}
 
-var pagos = parseInt(prompt("Ingrese la cantidad de cuotas (3, 6, 12, 24):"));
+let pagos;
 
-if (pagos === 3 || pagos === 6 || pagos === 12 || pagos === 24) {
-  
-    var montoTotalTarjeta = parseFloat(prompt("Ingrese el monto total de la tarjeta de crédito:"));
+while (true) {
+    pagos = parseInt(prompt("Ingrese la cantidad de cuotas (3, 6, 12, 24):"));
 
-    var cuotaMensualTarjeta = cuotasTarjeta(montoTotalTarjeta, pagos);
+    if (pagos === 3 || pagos === 6 || pagos === 12 || pagos === 24) {
+        break; // Sale del bucle si la entrada es válida
+    } else {
+        alert("No disponemos de esa cantidad de cuotas. Por favor, ingrese una opción válida.");
+    }
+}
 
-    alert("La cuota mensual de la tarjeta de crédito es: $" + cuotaMensualTarjeta);} 
-else {
-    alert("No tenemos esa cantidad de cuotas");}
+let montoTotalTarjeta = parseFloat(prompt("Ingrese el monto total de la tarjeta de crédito:"));
+
+let cuotaMensualTarjeta = cuotasTarjeta(montoTotalTarjeta, pagos);
+
+alert("La cuota mensual de la tarjeta de crédito es: $" + cuotaMensualTarjeta);
 
 
 
